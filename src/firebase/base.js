94d8639707ab367ab1a84b7887a7c2c99c1815b6
Soxsan <fredemey@gmail.com>
2025-04-18 17:45:32 +0200

    Update base.js

--- src/firebase/base.js
+++ src/firebase/base.js
@@ -1,19 +1,17 @@
-import Rebase from "re-base";
-import firebase from "firebase";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwlGxZRCqU23l44X0Cl3t7OoaHNdzJgV4",
+  authDomain: "battlevote-69f2a.firebaseapp.com",
+  projectId: "battlevote-69f2a",
+  storageBucket: "battlevote-69f2a.firebasestorage.app",
+  messagingSenderId: "32202533351",
+  appId: "1:32202533351:web:1a60e33eca026630d7f542"
 };
 
-export const endpoints = {
-  votes: "SCHWARZPOLL_VOTES",
-  questions: "SCHWARZPOLL_QUESTIONS"
-};
-
-export const firebaseApp = firebase.initializeApp(config);
-export const base = Rebase.createClass(firebaseApp.database());
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
